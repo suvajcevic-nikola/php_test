26e56283e46f453dbbbb3ce548368681d5ae5fa9
suvajcevic-nikola <suvajcevic1312@gmail.com>
2020-01-27 10:19:04 +0100

    Add database and config files.

---
+++ database.php
@@ -0,0 +1,44 @@
+<?php
+
+    $servername =  'localhost';
+    $username = 'root';
+    $password = '';
+
+    $conn = new mysqli($servername, $username, $password);
+    if($conn->connect_error)
+    {
+        die('Connection error: ' . $conn->connect_error);
+    }
+
+    echo 'Connection successful';
+
+    $sql = "CREATE DATABASE IF NOT EXISTS test CHARACTER SET utf16";
+
+    echo '<br>';
+    if($conn->query($sql))
+    {
+        echo 'Database "test" is created successfuly';
+    }
+    else
+    {
+        echo 'Error:' . $conn->error;
+    }
+
+    $sql = 'USE test;';
+
+    $sql = $sql . "CREATE TABLE IF NOT EXISTS users(
+                    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    username VARCHAR(50) NOT NULL,
+                    email VARCHAR(255) NOT NULL,
+                    password VARCHAR(255) NOT NULL
+                    );";
+
+    echo '<br>';
+    if($conn->multi_query($sql))
+    {
+        echo 'Table "users" is created succesfuly';
+    }
+    else
+    {
+        echo 'Error: ' . $conn->error;
+    }
